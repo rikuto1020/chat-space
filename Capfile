241f8bf234648788a198bb64a30a90fe25c37a2d
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-08-16 22:45:46 +0900

    Update

--- Capfile
+++ Capfile
@@ -9,10 +9,6 @@ require 'capistrano3/unicorn'
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
 
 # Load DSL and set up stages
-require "capistrano/setup"
-
-# Include default deployment tasks
-require "capistrano/deploy"
 
 # Load the SCM plugin appropriate to your project:
 #
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,15 +8,19 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: [' ~/.ssh/chatspace.pem']
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+                  set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+                  # Unicornの設定ファイルの場所
+                  set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+                  set :keep_releases, 5
+                  
+                  # デプロイ処理が終わった後、Unicornを再起動するための記述
+                  after 'deploy:publishing', 'deploy:restart'
+                  namespace :deploy do
+                    task :restart do
+                      invoke 'unicorn:restart'
+                    end
+                  end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
